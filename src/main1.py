db054f2de9f993539b0659d18048527080ab9fc8
Yaroslava Krenevych <yakrenevych@gmail.com>
2024-02-27 22:12:30 +0200

    click = circle

--- src/main1.py
+++ src/main1.py
@@ -1,17 +1,29 @@
 from tkinter import *
+from math import *
 
 root = Tk()
-r = 40
+r = 25
+cord = []
 
 # Створюємо полотно
-canvas = Canvas(root, width=400, height=300)
+canvas = Canvas(root, width=800, height=800)
 canvas.pack(side=LEFT)
+
+
+def checkDist(cur):
+    for el in cord:
+        if (el[0] - cur[0]) ** 2 + (el[1] - cur[1]) ** 2 < 4 * (r + 5) ** 2:
+            return False
+
+    return True
 
 
 def onCanvasClick(ev: Event):
     print(ev)
 
-    canvas.create_oval(ev.x - r, ev.y - r, ev.x + r, ev.y + r)
+    if checkDist((ev.x, ev.y)):
+        cord.append((ev.x, ev.y))
+        canvas.create_oval(ev.x - r, ev.y - r, ev.x + r, ev.y + r)
 
 
 canvas.bind('<Button-1>', onCanvasClick)
