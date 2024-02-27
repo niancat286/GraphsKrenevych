7621c99b9acc7493890a05c0b1e7afae96b659ad
Yaroslava Krenevych <yakrenevych@gmail.com>
2024-02-27 23:05:08 +0200

    + color, number of tops

--- src/main1.py
+++ src/main1.py
@@ -3,6 +3,7 @@ from tkinter import *
 
 root = Tk()
 r = 25
+numOfTop = 1
 cord = []
 
 
@@ -12,18 +13,22 @@ canvas.pack(side=LEFT)
 
 def checkDist(cur):
     for el in cord:
-        if (el[0] - cur[0]) ** 2 + (el[1] - cur[1]) ** 2 < 4 * (r + 5) ** 2:
+        if (el[0] - cur[0]) ** 2 + (el[1] - cur[1]) ** 2 < 4 * (r + 2) ** 2:
             return False
 
     return True
 
 
 def onCanvasClick(ev: Event):
+    global numOfTop
     print(ev)
+
 
     if checkDist((ev.x, ev.y)):
         cord.append((ev.x, ev.y))
-        canvas.create_oval(ev.x - r, ev.y - r, ev.x + r, ev.y + r)
+        canvas.create_oval(ev.x - r, ev.y - r, ev.x + r, ev.y + r, fill = 'lime')
+        canvas.create_text(ev.x, ev.y, text = str(numOfTop))
+        numOfTop += 1
 
 
 canvas.bind('<Button-1>', onCanvasClick)
